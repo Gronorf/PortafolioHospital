575d468260bd5300a04644deb1e3fc21752e9621
Mauricio Gutierrez Ulloa <121682144+Gronorf@users.noreply.github.com>
2023-05-04 16:14:45 -0400

    init

---
+++ hospital/src/test/java/portafolio/unab/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package portafolio.unab.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
